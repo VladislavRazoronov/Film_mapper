544962c1822bb0edc20d1a478927ab44c1d58c0b
Vladislav Razoronov <razoronov@ucu.edu.ua>
2020-02-18 19:17:02 +0200

    update

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 from pyrecord import Record
 from geopy import Nominatim
+from geopy.exc import GeocoderTimedOut
 import folium
+from folium.plugins import MarkerCluster
 import time
 
 film_info = Record.create_type('film_info','name','location')
 
 def convert_to_coordinates(location):
     """
+    (str)->tuple
     Converts location name into its longitude
     and latitude
     >>> convert_to_coordinates('Coventry, West Midlands, England, UK')
     (52.4081812, -1.510477)
     """
-    geolocator = Nominatim(user_agent="my_app")
-    loc = geolocator.geocode(location)
+    geolocator = Nominatim(user_agent='film_mapper')
     time.sleep(1)
+    try:
+        loc = geolocator.geocode(location)
+    except GeocoderTimedOut:
+        return convert_to_coordinates(location)
     if loc == None:
         return None
     return (loc.latitude,loc.longitude)
 
 def read_locations(year,filename):
     """
+    (str)->year
     Reads film years,names and locations from file
     """
     with open(filename,encoding='utf-8',errors='ignore') as file:
@@ -49,8 +56,11 @@ def find_closest(coordinates,films):
     (tuple, list)->tuple
     Finds closest film location to given coordinates
     and removes element from list
+    >>> find_closest((22,22),[film_info('filmname',(13,45)),film_info('namefilm',(20,21))])
+    film_info('namefilm',(20,21))
     """
-    dist_min = 10000000000000000000000000
+    dist_min = (films[0].location[0]-coordinates[0])**2 + (films[0].location[1]-coordinates[1])**2
+    el_min = films[0]
     for el in films:
         dist = (el.location[0]-coordinates[0])**2 + (el.location[1]-coordinates[1])**2
         if dist < dist_min:
@@ -69,11 +79,14 @@ if __name__ == '__main__':
     closest_films = []
     print('Adding markers...')
     markers = folium.FeatureGroup('Markers')
+    grid = MarkerCluster(name='Marker density')
     for i in range(10 if len(films)>10 else len(films)):
         closest_films.append(find_closest(loc,films))
     for el in closest_films:
         folium.Marker(el.location,el.name).add_to(markers)
+        folium.Marker(el.location,el.name).add_to(grid)
     film_map.add_child(markers)
+    film_map.add_child(grid)
     folium.LayerControl().add_to(film_map)
     film_map.save('Film_map.html')
     print("Finished, open Film_map.html for result")
